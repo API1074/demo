de8b3b7fbde28f49407b7e9068a4c172831c45c7
sonishreyas <sonishreyas10@gmail.com>
2021-04-06 14:35:17 +0530

    This is 2nd commit

--- demo.java
+++ demo.java
@@ -1,6 +1,7 @@
 public class demo{
 	public static void main(String args[]){
 	   System.out.println("Here is first line");
+   	   System.out.println("Here is second line,from next branch");
    }
 }     
 
