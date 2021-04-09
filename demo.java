92948eadd7141c263a4a44c7768c9d7e22833434
sonishreyas <sonishreyas10@gmail.com>
2021-04-09 13:40:47 +0530

    test pipeline

--- demo.java
+++ demo.java
@@ -2,9 +2,9 @@ public class demo{
 	public static void main(String args[]){
 	   System.out.println("Here is first line");
    	   System.out.println("Here is second line,from next branch");
-   	   System.out.println("Jenkins test");
-   	   System.out.println("Jenkins test2");
-   	   System.out.println("Jenkins test final");
+   	   System.out.println("Jenkins test 555");
+   	   System.out.println("Jenkins test 666");
+   	   
    }
 }     
 
